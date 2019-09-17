f09541cb8df9480ad776c8060617fbe60823fd65
NT-AndrewGosselin <andrew.gosselin@ntautoservices.com>
2019-09-17 08:12:44 -0400

    Removed Config File Information
    Accidently uploaded some configuration file information into the GIT.

--- src/config/sso.php
+++ src/config/sso.php
@@ -1,8 +1,8 @@
 <?php
 return [
-		'sso_url' => 'https://ssodev.newtechautomotiveservices.com', // The api url for SSO. ( EX :: "https://example.com" )
-        'login_route' => '/login', // Route where you want the login ( EX :: "/login" )
-        'logout_route' => '/logout', // Route where you want the logout ( EX :: "/logout" )
-        'home_route' => '/dashboard', // Route to your home page. ( EX :: "/home" )
-        'product_id' => 'Mg==', // You can grab this from the SSO website. ( EX :: "wQ==" )
+		'sso_url' => '', // The api url for SSO. ( EX :: "https://example.com" )
+        'login_route' => '', // Route where you want the login ( EX :: "/login" )
+        'logout_route' => '', // Route where you want the logout ( EX :: "/logout" )
+        'home_route' => '', // Route to your home page. ( EX :: "/home" )
+        'product_id' => '', // You can grab this from the SSO website. ( EX :: "wQ==" )
 ];
