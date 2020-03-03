778a6ecefce3a06144e4a899bea48dabc2d92d3b
Scott Lewis <scott.lewis@ntautoservices.com>
2020-03-03 11:07:24 -0500

    Expire time, no redirect if auth but no access

--- src/app/Http/Middleware/SSOAuthenticate.php
+++ src/app/Http/Middleware/SSOAuthenticate.php
@@ -8,6 +8,9 @@ use Illuminate\Http\Request;
 class SSOAuthenticate extends Authenticate
 {
     public function redirectTo($request){
+        if($request->session()->has('sso') && $request->session()->has('sso.id')){
+            abort(403, 'Acess denied');
+        }
         $request->session()->put('sso.authLeft', $request->fullUrl());
         return config('sso.authentication_url').'/remote/'.config('sso.id').'?returnTo="'.base64_encode($request->root()).'"';
     }
