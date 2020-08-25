fb066fec3ac9cffa54a663dc22e7511142b17fcf
Scott Lewis <scott.lewis@ntautoservices.com>
2020-08-25 14:12:34 -0400

    fixed routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,6 @@
 <?php
-Route::get('/ssobridge/setToken/{token}', 'Newtech\SSOBridge\App\Http\Controllers\SSOController@get_setToken')->middleware('web');
-Route::get('/ssobridge/logout', 'Newtech\SSOBridge\App\Http\Controllers\SSOController@logout')->middleware('web')->middleware('auth:sso')->name('sso.logout');
-Route::get('/ssobridge/changeUser/{id}', 'Newtech\SSOBridge\App\Http\Controllers\SSOController@changeUser')->middleware('web')->middleware('auth:sso')->name('sso.change');
-Route::get('/ssobridge/refresh', 'Newtech\SSOBridge\App\Http\Controllers\SSOController@refreshPermissions')->middleware('web')->name('sso.refresh');
-Route::get('/ssobridge/login', 'Newtech\SSOBridge\App\Http\Controllers\SSOController@get_setToken')->middleware('web')->middleware('auth:sso')->name('sso.login');
+Route::get('/ssobridge/setToken/{token}', 'Newtech\SSOBridge\SSOController@get_setToken')->middleware('web');
+Route::get('/ssobridge/logout', 'Newtech\SSOBridge\SSOController@logout')->middleware('web')->middleware('auth:sso')->name('sso.logout');
+Route::get('/ssobridge/changeUser/{id}', 'Newtech\SSOBridge\SSOController@changeUser')->middleware('web')->middleware('auth:sso')->name('sso.change');
+Route::get('/ssobridge/refresh', 'Newtech\SSOBridge\SSOController@refreshPermissions')->middleware('web')->name('sso.refresh');
+Route::get('/ssobridge/login', 'Newtech\SSOBridge\SSOController@get_setToken')->middleware('web')->middleware('auth:sso')->name('sso.login');
