3480d3455e0b0c670a61df9df4da0c6e633a7778
Scott Lewis <scott.lewis@ntautoservices.com>
2020-09-30 09:37:45 -0400

    allowed for default auth without broken redirects

--- src/SSOAuthenticate.php
+++ src/SSOAuthenticate.php
@@ -2,19 +2,37 @@
 
 namespace Newtech\SSOBridge;
 
-use \Illuminate\Auth\Middleware\Authenticate;
-use Illuminate\Http\Request;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Support\Str;
 
 class SSOAuthenticate extends Authenticate
 {
-    public function redirectTo($request){
+    /**
+     * Handle an unauthenticated user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $guards
+     * @return void
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    protected function unauthenticated($request, array $guards)
+    {
+        throw new AuthenticationException(
+            'Unauthenticated.', $guards, $this->redirectTo($request,$guards)
+        );
+    }
 
-        if($request->session()->has('sso') && $request->session()->has('sso.id')){
-            session()->pull('sso');
-            abort(403, 'Acess denied');
+    public function redirectTo($request, $guards = []){
+        if(in_array('sso', $guards)){
+            if($request->session()->has('sso') && $request->session()->has('sso.id')){
+                session()->pull('sso');
+                abort(403, 'Acess denied');
+            }
+            $request->session()->put('sso.authLeft', $request->fullUrl());
+            return Str::finish(config('sso.authentication_url'), '/').'remote/'.config('sso.id').'?returnTo="'.base64_encode($request->root()).'"';
         }
-        $request->session()->put('sso.authLeft', $request->fullUrl());
-        return Str::finish(config('sso.authentication_url'), '/').'remote/'.config('sso.id').'?returnTo="'.base64_encode($request->root()).'"';
+        return parent::redirectTo($request);
     }
 }
